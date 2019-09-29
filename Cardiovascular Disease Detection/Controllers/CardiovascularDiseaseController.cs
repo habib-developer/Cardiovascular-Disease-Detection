11732da2dda4f995126d6b770bd50417a607adef
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-09-29 21:32:33 +0500

    Add project files.

---
+++ Cardiovascular Disease Detection/Controllers/CardiovascularDiseaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cardiovascular_Disease_Detection.Models;
+using Cardiovascular_Disease_DetectionML.Model;
+
+namespace Cardiovascular_Disease_Detection.Controllers
+{
+    public class CardiovascularDiseaseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Predict()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Predict(ModelInput input)
+        {
+            var prediction = ConsumeModel.Predict(input);
+            ViewBag.Result = prediction;
+            return View();
+        }
+    }
+}
---
+++ Cardiovascular Disease Detection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cardiovascular_Disease_Detection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
